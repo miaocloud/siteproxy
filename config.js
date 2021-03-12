eb37fde9a8ef079839d0213ca0b1711d21de52d3
猫语花香 <35602616+miaocloud@users.noreply.github.com>
2021-03-12 11:07:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-zeta.vercel.app',
+    serverName: 'siteproxy-one-nu.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
